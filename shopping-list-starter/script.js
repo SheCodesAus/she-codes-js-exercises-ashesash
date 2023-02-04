a85a2e43dd34387a5e149123a59eb3bacb2a8281
ashesash <aishwarya.s@live.com>
2023-02-04 11:14:00 +1100

    made shopping list app

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,34 @@
-function addItem() {}
+let shoppingListItems = ["milk", "bread", "eggs"];
+let listElement = document.getElementById("shopping-list-items");
 
-function updateItems() {}
+shoppingListItems.forEach((item) => {
+//     console.log(item);
+//     console.log("buy a: " + item);
+//     console.log("should buy a:", item);
+//     console.log(`must buy a: ${item}`);
+    let itemElement = document.createElement("li");
+    itemElement.innerText = item;
+    listElement.appendChild(itemElement);
+});
+
+function addItem() {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems.push(item);
+    console.log("before update item :" , shoppingListItems);
+    updateItems();
+    console.log("after update item :" , shoppingListItems);
+}
+
+function updateItems() {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
+    shoppingListItems.forEach((item) => {
+        //     console.log(item);
+        //     console.log("buy a: " + item);
+        //     console.log("should buy a:", item);
+        //     console.log(`must buy a: ${item}`);
+            let itemElement = document.createElement("li");
+            itemElement.innerText = item;
+            listElement.appendChild(itemElement);
+        });
+}
